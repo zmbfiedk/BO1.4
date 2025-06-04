58045b6bf079e4d72d79b97131821dfe20cd8dc1
Arthur <lotuslovesurmom@gmail.com>
2025-06-04 12:47:59 +0200

    EnemySpawnerFix+working enemycount
    A lot Help

--- Assets/Scripts/Arthur/EnemySpawner.cs
+++ Assets/Scripts/Arthur/EnemySpawner.cs
@@ -1,7 +1,10 @@
+using System;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public static event Action OnEnemySpawn;
+
     [Header("Level 1")]
     [SerializeField] private GameObject Lmeleeenemyprefab1;
     [SerializeField] private GameObject Hmeleeenemyprefab1;
@@ -21,59 +24,59 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float maximumspawntime;
 
     private float timeTillSpawn;
-
     private WaveChecker waveChecker;
-
+    [SerializeField] private bool canSpawm = true;
+    [SerializeField] private int level;
     void Awake()
     {
         waveChecker = FindObjectOfType<WaveChecker>();
         SetTimeUntilSpawn();
     }
 
+    private void Start()
+    {
+        WaveChecker.OnMaxEnemySpawn += StopSpawn;
+        WaveChecker.OnWaveOver += StopSpawn;
+        WaveChecker.OnWaveOver += () => Invoke(nameof(AllowSpawning), 2f); 
+    }
+
     void Update()
     {
-        if (!waveChecker.IsWaveActive()) return; 
+        if (!waveChecker.IsWaveActive()) return;
         timeTillSpawn -= Time.deltaTime;
 
-        if (timeTillSpawn <= 0)
+        if (timeTillSpawn <= 0 && canSpawm)
         {
             SpawnEnemy();
             SetTimeUntilSpawn();
         }
     }
 
-
     private void SetTimeUntilSpawn()
     {
-        timeTillSpawn = Random.Range(minimumspawntime, maximumspawntime);
+        timeTillSpawn = UnityEngine.Random.Range(minimumspawntime, maximumspawntime);
     }
 
     private void SpawnEnemy()
     {
         int currentWave = waveChecker.GetWaveNumber();
-
-        int level = 1 + (currentWave / 10); 
-        level = Mathf.Clamp(level, 1, 3);   
+        int level = 1 + (currentWave / 10);
+        level = Mathf.Clamp(level, 1, 3);
         GameObject enemyToSpawn = null;
 
-        int enemyType = Random.Range(0, 3);
+        int enemyType = UnityEngine.Random.Range(0, 3);
+        OnEnemySpawn?.Invoke();
 
         switch (level)
         {
             case 1:
-                if (enemyType == 0) enemyToSpawn = Lmeleeenemyprefab1;
-                else if (enemyType == 1) enemyToSpawn = Hmeleeenemyprefab1;
-                else enemyToSpawn = Rangeenemyprefab1;
+                enemyToSpawn = enemyType == 0 ? Lmeleeenemyprefab1 : enemyType == 1 ? Hmeleeenemyprefab1 : Rangeenemyprefab1;
                 break;
             case 2:
-                if (enemyType == 0) enemyToSpawn = Lmeleeenemyprefab2;
-                else if (enemyType == 1) enemyToSpawn = Hmeleeenemyprefab2;
-                else enemyToSpawn = Rangeenemyprefab2;
+                enemyToSpawn = enemyType == 0 ? Lmeleeenemyprefab2 : enemyType == 1 ? Hmeleeenemyprefab2 : Rangeenemyprefab2;
                 break;
             case 3:
-                if (enemyType == 0) enemyToSpawn = Lmeleeenemyprefab3;
-                else if (enemyType == 1) enemyToSpawn = Hmeleeenemyprefab3;
-                else enemyToSpawn = Rangeenemyprefab3;
+                enemyToSpawn = enemyType == 0 ? Lmeleeenemyprefab3 : enemyType == 1 ? Hmeleeenemyprefab3 : Rangeenemyprefab3;
                 break;
         }
 
@@ -81,5 +84,15 @@ public class EnemySpawner : MonoBehaviour
         {
             Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
         }
+    }
+
+    private void StopSpawn()
+    {
+        canSpawm = false;
+    }
+
+    private void AllowSpawning()
+    {
+        canSpawm = true;
     }
 }
