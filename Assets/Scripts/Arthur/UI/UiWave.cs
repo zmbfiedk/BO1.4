c6898accd1de9b6c8e4ae3482642d60159cb378e
Arthur <lotuslovesurmom@gmail.com>
2025-06-12 17:45:17 +0200

    Uiwave Not done

---
+++ Assets/Scripts/Arthur/UI/UiWave.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEditor.Search;
+public class UiWave : MonoBehaviour
+{
+    [SerializeField] WaveChecker waveChecker;
+    TMPro.TextMeshPro textMeshPro;
+    [SerializeField] private int wavenmr;
+
+    void Start()
+    {
+        textMeshPro = GetComponent<TextMeshPro>();
+        wavenmr = waveChecker.WAVE;
+    }
+
+
+    void Update()
+    {
+        if (waveChecker.WAVE != wavenmr)
+        {
+            waveCouritine();
+            wavenmr = waveChecker.WAVE;
+        }
+    }
+
+    IEnumerator waveCouritine()
+    {
+        textMeshPro.text = "WAVE = " + waveChecker.WAVE.ToString("F1");
+        yield return new WaitForSeconds(9f);
+        textMeshPro.text = "";
+    }
+}
